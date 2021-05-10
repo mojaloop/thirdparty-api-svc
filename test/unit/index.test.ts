5fa92427f2880d63b8c6a53812f632961123a129
Kevin Leyow <kleyow@gmail.com>
2021-05-10 06:08:31 -0500

    chore: update package and fix tests

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -9,10 +9,10 @@
  --------------
  This is the official list of the Mojaloop project contributors for this file.
  Names of the original copyright holders (individuals or organizations)
- should be listed with a '*' in the first column. People who have
+ must be listed with a '*' in the first column. People who have
  contributed from an organization can be listed under the organization
  that actually holds the copyright for their contributions (see the
- Gates Foundation organization for an example). Those individuals should have
+ Gates Foundation organization for an example). Those individuals must have
  their names indented and be marked with a '-'. Email address can be added
  optionally within square brackets <email>.
  * Gates Foundation
@@ -57,7 +57,7 @@ const consentsRequestError = mockData.consentsThirdpartyError
 const patchThirdpartyTransactionIdRequest = mockData.patchThirdpartyTransactionIdRequest
 
 describe('index', (): void => {
-  it('should have proper layout', (): void => {
+  it('must have proper layout', (): void => {
     expect(typeof index.server).toBeDefined()
     expect(typeof index.server.run).toEqual('function')
   })
@@ -153,7 +153,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'transactionRequestId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'transactionRequestId\''
           }
         }
         const response = await server.inject(request)
@@ -233,7 +233,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'transactionId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'transactionId\''
           }
         }
         const response = await server.inject(request)
@@ -293,7 +293,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'errorInformation\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'errorInformation\''
           }
         }
         const response = await server.inject(request)
@@ -373,7 +373,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3100',
-            errorDescription: 'Generic validation error - .requestBody.status should be equal to one of the allowed values'
+            errorDescription: 'Generic validation error - /requestBody/status must be equal to one of the allowed values'
           }
         }
 
@@ -406,7 +406,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'sourceAccountId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'sourceAccountId\''
           }
         }
 
@@ -489,7 +489,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3100',
-            errorDescription: 'Generic validation error - .requestBody.status should be equal to one of the allowed values'
+            errorDescription: 'Generic validation error - /requestBody/status must be equal to one of the allowed values'
           }
         }
 
@@ -522,7 +522,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'sourceAccountId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'sourceAccountId\''
           }
         }
 
@@ -596,7 +596,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'consentId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'consentId\''
           }
         }
 
@@ -670,7 +670,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'initiatorId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'initiatorId\''
           }
         }
 
@@ -844,7 +844,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'initiatorId\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'initiatorId\''
           }
         }
 
@@ -906,7 +906,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'errorInformation\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'errorInformation\''
           }
         }
         const response = await server.inject(request)
@@ -978,7 +978,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'authToken\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'authToken\''
           }
         }
 
@@ -1053,7 +1053,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'type\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'type\''
           }
         }
 
@@ -1129,7 +1129,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'credential\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'credential\''
           }
         }
 
@@ -1203,7 +1203,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'errorInformation\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'errorInformation\''
           }
         }
         const response = await server.inject(request)
@@ -1318,7 +1318,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody[0] should have required property \'currency\''
+            errorDescription: 'Missing mandatory element - /requestBody/0 must have required property \'currency\''
           }
         }
 
@@ -1384,7 +1384,7 @@ describe('index', (): void => {
         const expected = {
           errorInformation: {
             errorCode: '3102',
-            errorDescription: 'Missing mandatory element - .requestBody should have required property \'errorInformation\''
+            errorDescription: 'Missing mandatory element - /requestBody must have required property \'errorInformation\''
           }
         }
         const response = await server.inject(request)
