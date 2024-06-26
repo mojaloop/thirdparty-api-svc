38e978b3237f121517cb72522aed6abceed2767a
Steven Oderayi <oderayi@gmail.com>
2024-06-26 15:56:41 +0100

    chore: update deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,8 @@
 
 module.exports = {
   reject: [
+    // upgrading eslint beyond v8.56.0 causes peer dependency conflict with @typescript-eslint/parser and @typescript-eslint/eslint-plugin
+    // can be upgrade when the dependent packages are updated
+    "eslint"
   ]
 }
