c9702dee973660f97b587423a9190ac67c08544e
Juan Correa <gibaros@users.noreply.github.com>
2025-02-26 07:33:43 -0500

    chore: maintenance updates (#97)
    * chore: maintenance updates
    
    -update dependencies
    -replace source code headers to Mojaloop Foundation
    
    * chore: sync yargs-parser

--- .ncurc.js
+++ .ncurc.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   reject: [
-    // upgrading eslint beyond v8.56.0 causes peer dependency conflict with @typescript-eslint/parser and @typescript-eslint/eslint-plugin
+    // upgrading eslint beyond v8.57.0 causes peer dependency conflict with @typescript-eslint/parser and @typescript-eslint/eslint-plugin
     // can be upgrade when the dependent packages are updated
     "eslint"
   ]
